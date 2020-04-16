bcbad099f8e4e438d195ff2fa24f6218b097e097
Sofia Velarde <sofiavelarde2004@gmail.com>
2020-04-16 11:31:08 -0700

    Encapsulation(IP)
    need to debug one test then finished

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -7,7 +7,7 @@ package _01_introduction_to_encapsulation;
  * To encapsulate a member variable, follow these three steps:
  * 
  * 1. Make the member variable private (or protected)
- * 
+ *
  * 2. Make a getter and setter method (accessor and mutator) for the variable.
  * 
  * 3. Add restrictions to the setter method so the member variable cannot be 
@@ -15,20 +15,68 @@ package _01_introduction_to_encapsulation;
  * 
  * */
 
-
 public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
-	
-	//2. Create a new JUnit Test case and write tests to verify that 
-	//   the member variables' getters and setters are working
-	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
-	
-	public static void main(String[] args) {
-		
+	// 1. Encapsulate the member variables.
+	// Add restrictions to the setters according to the comment.
+
+	// 2. Create a new JUnit Test case and write tests to verify that
+	// the member variables' getters and setters are working
+
+	private int itemsReceived; // must not be negative. All negative arguments get set to 0.
+	public int getItemsReceived() {
+		return itemsReceived;
 	}
+	public void setItemsReceived(int itemsReceived) {
+		if (itemsReceived < 0) {
+			itemsReceived = 0;
+		}
+		this.itemsReceived = itemsReceived;
+	}
+	
+	
+	
+	
+	private float degreesTurned; // must be locked between 0.0 and 360.0 inclusive.
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+	public void setDegreesTurned(float degreesTurned) {
+		if (degreesTurned < 0.0) {
+			degreesTurned = 0;
+		}else if(degreesTurned > 360.0) {
+			degreesTurned = 360;
+		}
+		this.degreesTurned = degreesTurned;
+	}
+	
+	
+	
+	private String nomenclature = " "; // must not be set to a blank string. Blank Strings get set to a space
+	public String getNomenclature() {
+		return nomenclature;
+	}
+	public void setNomenclature(String nomenclature) {
+		if (nomenclature.equals("")) {
+			nomenclature = " ";
+		}
+		this.nomenclature = nomenclature;
+	}
+	
+	
+	
+	private Object memberObj; // must not be a String. If it is a String, set it equal to a new Object();
+	public Object getMemberObj() {
+		return memberObj;
+	}
+	public void setMemberObj(Object memberObj) {
+		if (memberObj instanceof String) {
+			//Object newObj = new Object();
+			memberObj.equals(false);
+			
+		}
+		
+		
+		this.memberObj = memberObj;
+	}
+	
 }
---
+++ src/_01_introduction_to_encapsulation/EncapsulationTest.java
@@ -0,0 +1,45 @@
+package _01_introduction_to_encapsulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class EncapsulationTest {
+	@Test
+	void testItemsReceived(){
+		EncapsulateTheData encapsulate = new EncapsulateTheData();
+		encapsulate.setItemsReceived(4);
+		assertEquals(4, encapsulate.getItemsReceived());
+		encapsulate.setItemsReceived(-1);
+		assertEquals(0, encapsulate.getItemsReceived());
+	}
+	@Test
+	void testDegreesTurned(){
+		EncapsulateTheData encapsulate = new EncapsulateTheData();
+		encapsulate.setDegreesTurned(120);
+		assertEquals(120,encapsulate.getDegreesTurned());
+		encapsulate.setDegreesTurned(550);
+		assertEquals(360,encapsulate.getDegreesTurned());
+	}
+	@Test
+	void testNomenclature() {
+		EncapsulateTheData encapsulate = new EncapsulateTheData();
+		encapsulate.setNomenclature("Wow");
+		assertEquals("Wow", encapsulate.getNomenclature());
+		encapsulate.setNomenclature("");
+		assertEquals(" ", encapsulate.getNomenclature());
+		
+	}
+	
+	@Test
+	void testMemberObj() {
+		EncapsulateTheData encapsulate = new EncapsulateTheData();
+		encapsulate.setMemberObj(5);
+		assertEquals(5,encapsulate.getMemberObj());
+		encapsulate.setMemberObj("hello");
+		assertFalse(encapsulate.getMemberObj() instanceof String);
+		
+	}
+	
+	
+}
